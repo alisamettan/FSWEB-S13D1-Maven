89ad94137fef79b99f1b4f5d8a959631bfebc369
alisamettan <alisamet_tan@hotmail.com>
2024-03-18 14:20:57 +0300

    Yaş taski yapıldı.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -14,18 +14,28 @@ public class Main {
     }
 
     public static boolean hasTeen(int firstAge, int secondAge, int thirdAge) {
-       
+
+        int[] ages={firstAge,secondAge,thirdAge};
+
+        //foreach
+        for(int age:ages ){
+            if(age>=13 && age<=19){
+                return true;
+            }
+        }
+
+        return false;
     }
 
     public static boolean isCatPlaying(boolean isSummer, int temp) {
-     
+
     }
 
     public static double area(double width, double height) {
-       
+
     }
 
     public static double area(double radius) {
-       
+
     }
 }
